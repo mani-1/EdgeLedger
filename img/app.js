4870f48bf3d6974400c7cf95b36d8d2e536c3229
manikandan <manikandan.tms88@gmail.com>
2020-04-21 22:50:04 +0530

    edge ledger final outcome

---
+++ img/app.js
@@ -0,0 +1,53 @@
+ 
+
+/*   nav bar script  */
+
+const header =  document.querySelector('header');
+header.addEventListener('click',(e)=> {
+
+       if(e.target.tagName === 'I'){
+          document.querySelectorAll('li').forEach((list) => {
+                  list.classList.toggle('show-nav');     
+                  
+          })
+       }
+})
+
+/*    fade out script  */
+
+   window.addEventListener('scroll',()=> {
+       scrollFunction()
+   })  
+   function scrollFunction(){
+       if(document.documentElement.scrollTop > 100){
+            document.querySelector('header').style.backgroundColor = " rgba(3, 3, 3, 0.692) "
+       }
+    
+   }
+
+/*    smooth scrolling  script  */       
+
+$(document).ready(function(){
+// Add smooth scrolling to all links
+$("a").on('click', function(event) {
+
+// Make sure this.hash has a value before overriding default behavior
+if (this.hash !== "") {
+ // Prevent default anchor click behavior
+ event.preventDefault();
+
+ // Store hash
+ var hash = this.hash;
+
+ // Using jQuery's animate() method to add smooth page scroll
+ // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+ $('html, body').animate({
+   scrollTop: $(hash).offset().top -80
+ }, 800, function(){
+
+// Add hash (#) to URL when done scrolling (default click behavior)
+    
+   });
+} // End if
+});
+});
